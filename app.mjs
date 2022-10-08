74da52c7d524eba3b3ec58c911209719e36190ee
ABHIRUP <abhirupsaha02@gmail.com>
2022-10-08 21:14:22 +0530

    different countries' time

--- app.mjs
+++ app.mjs
@@ -1,4 +1,3 @@
-let app_wrap = document.querySelector('.app-wrap')
 let city = document.querySelector('.location .city')
 let date = document.querySelector('.location .date')
 let temp = document.querySelector('.current .temp')
@@ -9,6 +8,8 @@ let feels_like = document.querySelector('.feels-like')
 let humidity = document.querySelector('.humidity')
 let pressure = document.querySelector('.pressure')
 let wind_speed = document.querySelector('.wind-speed')
+let time = document.querySelector('.time')
+let curr_time = document.querySelector('p')
 
 const api = {
     key: "150740df546d39e577ffd82527b393a8",
@@ -19,9 +20,10 @@ const searchbox = document.querySelector('.search-box')
 searchbox.addEventListener('keypress', (event) => {
     if (event.keyCode == 13) {
         getResults(searchbox.value)
+        if (currentTime) clearInterval(currentTime)
     }
 })
-
+import timezone from './node_modules/timezone-country-region/lib/timezone.mjs'
 function getResults(query) {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
@@ -29,8 +31,28 @@ function getResults(query) {
         }).then(displayResults)
 }
 
+let currentTime
 function displayResults(weather) {
+    console.log(timezone.lookup(weather.sys.country))
     console.log(weather)
+
+    // Time Builer
+    currentTime = setInterval(() => {
+        let text = new Date().toLocaleString("en-US", {
+            timeZone: `${timezone.lookup(weather.sys.country)}`,
+            timeStyle: "short",
+            hourCycle: "h24"
+        })
+        curr_time.textContent = text
+        time.style.opacity = '1'
+    }, 1000)
+
+    //If a region is not found from API
+    if (timezone.lookup(weather.sys.country) === undefined) {
+        time.style.opacity = '0'
+        clearInterval(currentTime)
+    }
+
     city.innerText = `${weather.name}, ${weather.sys.country}`
 
     let now = new Date()
@@ -46,18 +68,18 @@ function displayResults(weather) {
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/haze.webp')"
             break
-        
+
         case "Drizzle":
         case "Rain":
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/rain.webp')"
             break
-        
+
         case "Clouds":
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/clouds.webp')"
             break
-        
+
         case "Mist":
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/mist.webp')"
@@ -67,7 +89,7 @@ function displayResults(weather) {
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/fog.webp')"
             break
-        
+
         case "Clear":
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/clear.webp')"
@@ -79,7 +101,7 @@ function displayResults(weather) {
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/thunderstorm.webp')"
             break
-        
+
         default:
             weather_el.innerText = weather.weather[0].description
             document.body.style.backgroundImage = "url('./Images/bg.webp')"
@@ -88,7 +110,7 @@ function displayResults(weather) {
     if (weather.weather[0].main != 'Clear') {
         document.body.style.backgroundPositionY = null
     }
-    
+
     hilow.innerText = `↿ ${Math.round(weather.main.temp_max)}°C\u00A0\u00A0\u00A0⇃ ${Math.round(weather.main.temp_min)}°C`
 
     document.querySelector('#w-icon').src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
@@ -98,7 +120,7 @@ function displayResults(weather) {
     feels_like.innerHTML = `Feels like\u00A0\u00A0\u00A0${Math.round(weather.main.feels_like)}°C`
     humidity.innerHTML = `Humidity\u00A0\u00A0\u00A0${weather.main.humidity}%`
     pressure.innerHTML = `Pressure ${(weather.main.pressure * 0.00750062).toFixed(1)}mm Hg`
-    wind_speed.innerHTML = `Wind ${Math.round(weather.wind.speed*18/5)} km/h`
+    wind_speed.innerHTML = `Wind ${Math.round(weather.wind.speed * 18 / 5)} km/h`
 
 }
 
